9c95c6bc839095cfd49a5c1450c637baf13bab60
Alao046 <alaoqud77@gmail.com>
2026-08-14 04:56:39 +0100

    asynchronous mails integartion

--- src/main/java/com/justjava/humanresource/utils/AfterCommitExecutor.java
+++ src/main/java/com/justjava/humanresource/utils/AfterCommitExecutor.java
@@ -1,13 +1,22 @@
 package com.justjava.humanresource.utils;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.support.TransactionSynchronization;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import java.util.concurrent.Executor;
+
 @Slf4j
 @Component
 public class AfterCommitExecutor {
+
+    private final Executor notificationTaskExecutor;
+
+    public AfterCommitExecutor(@Qualifier("notificationTaskExecutor") Executor notificationTaskExecutor) {
+        this.notificationTaskExecutor = notificationTaskExecutor;
+    }
 
     public void runAfterCommit(Runnable runnable) {
         if (runnable == null) {
@@ -18,11 +27,19 @@ public class AfterCommitExecutor {
             TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
                 @Override
                 public void afterCommit() {
-                    runSafely(runnable);
+                    runAsync(runnable);
                 }
             });
         } else {
-            runSafely(runnable);
+            runAsync(runnable);
+        }
+    }
+
+    private void runAsync(Runnable runnable) {
+        try {
+            notificationTaskExecutor.execute(() -> runSafely(runnable));
+        } catch (RuntimeException e) {
+            log.warn("AfterCommitExecutor: could not schedule callback, ignoring: {}", e.getMessage(), e);
         }
     }
 
